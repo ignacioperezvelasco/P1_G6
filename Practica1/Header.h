d31d1b56ca3bc8758d1d724aff3d869a5f7b9433
ignacioperezvelasco <ignacioperezvelasco@S1-29>
2017-03-21 12:50:07 +0100

    esta

--- Practica1/Header.h
+++ Practica1/Header.h
@@ -1,21 +1,52 @@
 #pragma once
-class map
+#include <iostream>
+class Map
 {
 public:
-	int dificultad;
-	int inicializarmapa(int *dificultad) {}
-	void cambiarpunto(int x, int y, char nuevoelemento) {	}
-	void printmapa() {}
+	//atributos
+	int dificultad=2;
+	int numRows;
+	int numColumns;
+	int **md;
+
+	Map(int dificultad) :
+		dificultad(dificultad),
+		numRows(dificultad * 5),
+		numColumns(dificultad * 5*2)
+	{
+		md = new int *[numRows];
+		for (int i = 0; i < numRows; i++) {
+			md[i] = new int[numColumns];
+		}
+	}
+	void cambiarpunto(int x, int y, char nuevoelemento)
+	{
+		md[x][y] = nuevoelemento;
+	}
+
+	void printmapa()
+	{
+		for (int i = 0; i < numRows; i++)
+		{
+			for (int j = 0; j < numColumns; j++)
+			{
+				std::cout << md[i][j] << std::endl;
+			}
+		}
+	}
 };
 
-class player 
+class Player 
 {
-	int posactual[2];
-	int poscomprovar[2];
+	Map &dif:dificultad;
+	struct player {
+		int x;
+		int y;
+	};
 	//que celda comprovar(funcion en descripción practica)
 	void comprovar(int posicion[2]) {};
 };
-
+/*
 class coinManager 
 {
 	int score (int score) {
@@ -29,4 +60,4 @@ class coinManager
 	}
 	const int Monedas=((rand()%10 + 3)*map::dificultad);
 	void eliminarMonedas();	
-};
+};*/
---
+++ Practica1/main.cpp
@@ -0,0 +1,11 @@
+#include < iostream>
+#include "Header.h"
+
+int main() {
+
+	Map(1);
+	void Map::cambiarpunto(3, 4, '0');
+	void Map::printmapa();
+
+
+}
